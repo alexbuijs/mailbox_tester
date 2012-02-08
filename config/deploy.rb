f7203cdf163d30da2a1cd7543e16bac2908f3129
Alex Buijs <alex.buijs@ciber.nl>
2012-02-08 13:52:36 +0100

    Add capistrano

---
+++ config/deploy.rb
@@ -0,0 +1,23 @@
+set :application, "mailbox_tester"
+set :repository,  "git@github.com:alexbuijs/mailbox_tester.git"
+
+set :scm, :git
+# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :deploy_to, "/home/cvzprj/#{application}"
+set :user, "cvzprj"
+set :password, "prjcvz99"
+set :use_sudo, false
+
+server "CVZLACT001", :app, :web, :db, :primary => true
+
+# if you're still using the script/reaper helper you will need
+# these http://github.com/rails/irs_process_scripts
+
+# If you are using Passenger mod_rails uncomment this:
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
