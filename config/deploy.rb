0eb8469fb8656ffa94c02bda07068662aef5c330
Alex Buijs <alex.buijs@ciber.nl>
2012-02-08 21:38:30 +0100

    update deploy script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,20 @@
 require 'bundler/capistrano'
 load 'deploy/assets'
 
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"
+set :rvm_type, :user
+
+default_environment['LD_LIBRARY_PATH'] = "/usr/local/lib"
+
 set :application, "mailbox_tester"
 set :repository,  "https://github.com/alexbuijs/mailbox_tester.git"
 set :normalize_asset_timestamps, false
-
 set :scm, :git
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
 set :deploy_to, "/home/cvzprj/#{application}"
 set :user, "cvzprj"
 set :password, "prjcvz99"
 set :use_sudo, false
-
-default_environment['LD_LIBRARY_PATH'] = "/usr/local/lib"
-
-$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-require "rvm/capistrano"                  # Load RVM's capistrano plugin.
-set :rvm_type, :user
 
 server "CVZLACT001", :app, :web, :db, :primary => true
 
