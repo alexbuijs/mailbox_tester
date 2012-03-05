3cfed804188970d558e05b222b93dff9d9abdda9
Alex Buijs <alex.buijs@ciber.nl>
2012-03-05 08:09:59 +0100

    Show results for correct day

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -29,9 +29,7 @@ class ApplicationController < ActionController::Base
   def messages
     respond_to do |format|
       format.json do
-        date = params[:year] && params[:month] && params[:day] ? 
-          Date.parse("#{params[:year].to_i}-#{params[:month].to_i}-#{params[:day].to_i}") :
-          Date.today
+        date = Time.parse("#{params[:year].to_i}-#{params[:month].to_i}-#{params[:day].to_i}")
         respond_with Result.mismatch.from_date(date).scoped
       end
       format.html { render :index }
